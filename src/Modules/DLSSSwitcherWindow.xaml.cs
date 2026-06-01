ba36d5a96b825eb3b2b7b91071a0cd7d4690bfcf
Cubeir <cube.master.ir@outlook.com>
2026-06-01 17:18:42 +0100

    Update libs, remove system.automation

--- src/Modules/DLSSSwitcherWindow.xaml.cs
+++ src/Modules/DLSSSwitcherWindow.xaml.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
-using System.Management.Automation;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.UI;
