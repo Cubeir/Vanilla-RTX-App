9ccb6eb80d97c3e78eeae1a2438ed7c8feacc634
Cubeir <cube.master.ir@outlook.com>
2026-09-08 10:31:51 +0100

    Move Alchitex license to the root of modules' folder

--- src/Modules/Alchitex/AlchitexWindow.xaml.cs
+++ src/Modules/Alchitex/AlchitexWindow.xaml.cs
@@ -282,7 +282,7 @@ public sealed partial class Alchitex : Window
     {
         try
         {
-            var uri = new Uri("ms-appx:///Modules/Alchitex/Assets/LICENSE.txt");
+            var uri = new Uri("ms-appx:///Modules/Alchitex/ALCHITEX_LICENSE.txt");
             var file = await StorageFile.GetFileFromApplicationUriAsync(uri);
             var body = await FileIO.ReadTextAsync(file);
 
