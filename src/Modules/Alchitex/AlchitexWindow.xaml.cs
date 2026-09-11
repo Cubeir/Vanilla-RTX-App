7210ab6addf7fda821713e5f28449280e8a26981
Cubeir <cube.master.ir@outlook.com>
2026-09-11 12:20:53 +0100

    Update plans

--- src/Modules/Alchitex/AlchitexWindow.xaml.cs
+++ src/Modules/Alchitex/AlchitexWindow.xaml.cs
@@ -1817,5 +1817,46 @@ public sealed partial class Alchitex : Window
 
 
 /* ### BACKLOG/TODO OF ALCHITEX (HIGHLY CONFIDENTIAL)
- * 
+- Need a new dev tool to verify integrity of the materials.json file, and to TRIM IT DOWN!
+I.e. where it falls back to fallbacks anyway, remove the unneeded entries, could cut down the size of the file by 500kb or so,
+which is genuinely helpful when retrieving the new json from web.
+
+- Be smart with the values chosen for recursion
+An idea was, wheer confidence is high, higher mins are permissable, where confidence is lower, lower mins reduce error rate.
+Or also think into the "sway" idea you had earlier
+Cave vine berries, there is usually brown thrown in there, and you got oranges too.
+Both have EQUALLY TERRIBLE red dominance, BUT it should be favoring the ones with higher value, and I think it does, the current recursion does that...
+some tests will make it clearer.
+Dominance of a color in its own domain matters more than the height of the value.
+
+- A feature for inversing the rules of the recusion MIGHT come handy
+
+- Double-channel target? would be nice to target more specific colors, but it will inevitably expand the mouth of the recurser and increasing rate of mistakes.
+
+Note: MER recursion isn't too aggressive at suppressing other values in fact, it is perfect as it is, mayeb more aggressive would be better at reducing mistakes,
+making it more usable in wider scenarios at the cost of looking worse where confidence is high.
+
+- The normal map generator is a magnificent piece of work, but, it depends on the heightmap generation downstream,
+which could be improved.
+Also the decisionmaker of making things flatter the more noisy is a bit too aggressive and it is falsely suppressing normals it shouldn't.
+
+- Could have added something to objectively derive baseline normal and heightmap intensities from Vanilla RTX/Normals, get the median of the whole thing, calculate deviation from that median.
+Then uniformly adjust all, until a baseline is reached, though with manual alterations and tweaks this may no longer be necessary and make actually become counterproductive.
+
+- An idea leftover from the alchemist project was:
+Breaking down MER generation into four parts, each layer processed independantly of the other.
+Would make the pipeline a lot more digestible, and add room for more versatility within each part.
+Each layer gets its own recursion pass.
+Adds a sort of modularity that CAN help you elevate the quality of the whole thing.
+
+It also said that, maybe dealing with absolutes is wrong, instead try to develop something that SWAYS assignment of certain PBR properties towards certain channels.
+By "swaying" and concentrating certain properties towards certain colors (the more of them there is) it could help for instance
+Keep a diamond ore's grey parts MER composition INTACT, without any sacrifice, whilst swaying metalness towards Bluer parts.
+Then, all get composited independantly. Can produce Significantly more predictable results, with greater control over the final output.
+This is an alterantive way to go about the current recursion tactic, but it remains to be experimented with more thoroughly...
+
+It is probably a less generally-applicable idea. It might work better in places but you have to be considering ANY texture pack that might come its way
+and if some system ends up being so specific about some things that it degrades other scenarios (most other), then it is probably not a good idea.
+Still, it was worth recording here, maybe more thoughts develop on it down the line, or if you're a third-party reading this, feel free to chip in.
+
 */
